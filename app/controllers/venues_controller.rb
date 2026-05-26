3fbcbc3151473a42b0b42e7e3946ca6ab7825604
Dom Pukallus <dpukallus9@gmail.com>
2026-05-26 12:27:00 +1000

    add dotenv-rails for mapbox api token to be stored in

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,9 @@
 class VenuesController < ApplicationController
+  def index
+    @venues = Venue.all
+  end
+
+  def show
+    @venues = Venue.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :venues, only: [:index, :show] do
+    resources :logs, only: [:create]
+  end
 end
